3277ea9e57e4a1f0dfd6bfd362ff4a15e1f148c9
Kelvin Shen <kelvinshen@outlook.com>
2016-04-21 00:31:27 +1200

    main character can move obj 2

---
+++ Assignment2/Assets/Scripts/CollisionTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionTrigger : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Entered");
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Staying in Trigger");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Trigger Exited");
+    }
+}
